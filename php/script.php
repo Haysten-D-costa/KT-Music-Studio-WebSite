f4f0afba66129a7c0d0a97aabd99166c9e9080bd
Haysten-D-costa <haystendc@gmail.com>
2024-10-02 13:37:19 +0530

    final commit

--- php/script.php
+++ php/script.php
@@ -1,16 +1,44 @@
 <?php
-    $conn = mysqli_connect("localhost", "root", "");
+    $name = $_POST["first-name"]." ".$_POST["last-name"]; // text-box....
+    $email = $_POST["email"];
+    $dob = $_POST["dob"];
+    $gender = $_POST["gender"]; // radio-button....
+    $phoneno = $_POST["phoneno"];
+    $level = $_POST["level"];
     
-    if($conn) { echo "<br> Successful Connection !"; }
+    // check-box....
+    $instrument = $_POST["instrument"];
+    $instrument_selected = "";
+    for ($i=0; $i < count($instrument); $i++) {
+        $instrument_selected = $instrument_selected ." ". $instrument[$i];
+    }
+    $date = $_POST["date-time"];
+
+    echo "<br> Displaying Details : ";
+    echo "<br><br>Name : ". $name;
+    echo "<br>Email : ". $email;
+    echo "<br>Date of Birth : ". $dob;
+    echo "<br>Gender : ". $gender;
+    echo "<br>Phone no. : ". $phoneno;
+    echo "<br>Musician level : ". $level;
+    echo "<br>Instruments selected : ". $instrument_selected;
+    echo "<br>Date of commence : ". $date;
+
+    $conn = mysqli_connect("localhost", "root", "", "KMAcademy");
+    if($conn) { echo "<br><br>Connection successful !"; }
     else {
-        echo "<br> Failed Connection !";
+        echo "<br>Connection Failed !";
         exit();
     }
-    $query = "CREATE DATABASE KTMStudio";
+    $query = "
+        INSERT INTO student (name, email, dob, gender, phoneno, level, instruments, doc)
+        VALUES
+        ('$name', '$email', '$dob', '$gender', '$phoneno', '$level', '$instrument_selected', '$date');
+    ";
     $r = mysqli_query($conn, $query);
-    if($r) { echo "<br> Table created successfully !"; }
+    if($r) { echo "<br>Entry insertion successful !"; }
     else {
-        echo "<br> Table creation failed !";
+        echo "<br>Entry insertion failed !";
         exit();
     }
     mysqli_close($conn);
